b17642c22a63f0e7cdc008ec2ba430074c0db5d6
AlbertSnow <Albert.Snow.Email@gmail.com>
2020-12-04 09:38:56 +0800

    1. import opencv native library

--- app/src/main/java/site/albertsnow/opencv/MainActivity.kt
+++ app/src/main/java/site/albertsnow/opencv/MainActivity.kt
@@ -1,11 +1,21 @@
 package site.albertsnow.opencv
 
-import androidx.appcompat.app.AppCompatActivity
+import android.app.Activity
 import android.os.Bundle
+import android.view.View
+import android.widget.Toast
+import org.opencv.android.OpenCVLoader
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : Activity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
     }
+
+    fun testLoading(view: View) {
+        val success = OpenCVLoader.initDebug()
+        val message = if (success) "Loading Success" else "Loading Failure-----"
+        Toast.makeText(this, message, Toast.LENGTH_LONG).show()
+    }
+
 }
